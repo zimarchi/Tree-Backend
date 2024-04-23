fde37cac4de2726178a5b04bee5237e3745c5433
Nazim <nazim.messaoudene@pm.me>
2024-04-24 00:33:17 +0200

    tests jest sur backend

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const formatName = str => {
     return (typeof str === "string" ? str[0].toUpperCase() + str.slice(1).toLowerCase() : undefined)
 }
 
-module.exports = {formatName} 
+const addNewProperty = (obj, props, value) => {
+    if (typeof props !== "string" || value==undefined || value==null) return false
+    if (typeof obj === "object" && obj !== null && !Array.isArray(obj)) {
+        if (obj[props]) return false
+        obj[props]= value
+        return true
+    }
+    return false
+}
+
+module.exports = {formatName, addNewProperty} 
--- index.test.js
+++ index.test.js
@@ -1,4 +1,6 @@
-const {formatName} = require("./index.js");
+const {formatName, addNewProperty} = require("./index.js");
+
+//tests sur fonction formatName
 
 let res = formatName("nazim")
 let resUndefined = formatName()
@@ -12,4 +14,43 @@ test ("La fonction formatName renvoit un string egal à Nazim", () => {
 test ("Si argument non string alors formatName renvoit undefined", () => {
     expect (resUndefined).toBeUndefined()
     expect (resNumber).toBeUndefined()
+})
+
+//tests sur fonction addNewproperty
+
+let objToChange = {first: "Naz"}
+addNewProperty(objToChange, "last", "Mes")
+addNewProperty(objToChange, "first", "Naz2")
+
+let str = "toto"
+let koStr = addNewProperty(str, "props", "ko")
+
+let array = [2,3]
+let koArray = addNewProperty(array, "props", "ko")
+
+let koNum = addNewProperty(objToChange, 6, "ko")
+
+test ("La fonction addNewProperty renvoit un objet", () => {
+    expect (typeof objToChange).toBe("object")
+    expect (objToChange).not.toBe(null)
+    expect (Array.isArray(objToChange)).not.toBe(true)
+})
+
+test ("La fonction addNewProperty rajoute la propriété reçue", () => {
+    expect (objToChange.last).toBeTruthy()
+    expect (objToChange.last).toBeDefined()
+    expect (objToChange).toHaveProperty("last")
+})
+
+test ("La fonction addNewProperty n'écrase pas une propriété existante", () => {
+    expect (objToChange.first).toBe("Naz")
+})
+
+test ("Tous les arguments de la fonction addNewProperty doivent être corrects", () => {
+    expect (koStr).toBeFalsy()
+    expect (koArray).toBeFalsy()
+})
+
+test ("La props envoyée dans la fonction addNewProperty ne doit pas etre un nombre", () => {
+    expect (koNum).toBeFalsy()
 })
